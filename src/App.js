e8f1ab665cc5ec6dd4ebb7879957b428fd096089
cheseryon050822 <cseryon@gmail.com>
2021-09-18 18:46:42 +0900

    speed star

--- src/App.js
+++ src/App.js
@@ -5,10 +5,17 @@ const App = () => {
   const ann =()=>{
     console.log('Hello world')
   }
+  const open = ()=>{
+    window.alert("Hello world!");
+    alert("Hello world!");
+  }
   return (
     <div>
       <button onClick={ann}>
         押してください
+      </button>
+      <button onClick={open}>
+         上からくる
       </button>  
     </div>  
 
