223726236817b6cc9723f3ab61e3186b5317c6e1
cheseryon050822 <cseryon@gmail.com>
2021-09-25 22:37:08 +0900

    goldexp

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import Button from '@restart/ui/esm/Button';
+import React,{useState} from 'react'
+
 
 
 const App = () => {
+  const [count,setCount]=useState(0)
   const ann =()=>{
     console.log('Hello world')
   }
@@ -9,15 +12,31 @@ const App = () => {
     window.alert("Hello world!");
     alert("Hello world!");
   }
+  const wanabeeee =()=> {
+    setCount(count+1)
+　}
+  const ureeeyyy =()=>{
+    setCount(count-1)
+  }
+  const overdrive =()=>{
+     setCount(count*0)
+  }
+
   return (
     <div>
       <button onClick={ann}>
         押してください
       </button>
-    　
+      
       <button onClick={open}>
          上からくる
       </button>  
+      <div>
+        <Button onClick={wanabeeee}>いいね</Button>
+        <Button onClick={ureeeyyy}>ダメだね</Button>
+        <Button onClick={overdrive}>reset</Button>
+        {count}
+      </div>
     </div>  
 　　
       
