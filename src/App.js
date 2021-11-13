4653dd2a9177538193034028367bdaf1460f71a1
cheseryon050822 <cseryon@gmail.com>
2021-11-13 18:57:25 +0900

    axios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Button from '@restart/ui/esm/Button';
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import "./App.css"
+import axios from 'axios'
 
 const App = () => {
   const [count,setCount]=useState(0)
@@ -9,6 +10,8 @@ const App = () => {
   const [scissor,setScissor]=useState("チョキ")
   const [paper,setPaper]=useState("パー")
   const[myhand,setMyhand]=useState("")
+  const[judge,setJudge]=useState()
+
   const ann =()=>{
     console.log('Hello world')
   }
@@ -28,6 +31,31 @@ const App = () => {
   const stand =()=>{
     setCount(count*5)
   }
+  useEffect(() => {
+    winorlose()
+  })
+  
+const winorlose=()=>{
+    if(myhand==="グー" && cpuhand==="チョキ") {
+     setJudge('勝ち！')
+   }else if(myhand==="グー" && cpuhand==="パー"){
+     setJudge('負け…')
+   }else if(myhand==="グー" && cpuhand==="グー"){
+     setJudge('引き分け')
+   }else if(myhand==="チョキ" && cpuhand==="パー"){
+     setJudge('勝ち！')
+   }else if(myhand==="チョキ" && cpuhand==="グー"){
+     setJudge('負け…')
+   }else if(myhand==="チョキ" && cpuhand==="チョキ"){
+     setJudge('引き分け')
+   }else if(myhand==="パー" && cpuhand==="チョキ"){
+     setJudge('負け…')
+   }else if(myhand==="パー" && cpuhand==="グー"){
+     setJudge('勝ち！')
+   }else if(myhand==="パー" && cpuhand==="パー"){
+     setJudge('引き分け')
+   }
+ }
   const [texter,setTexter]=useState()
   const [maseege,setMseege]=useState()
   const eirian=()=>{
@@ -57,13 +85,21 @@ const App = () => {
     setMyhand("パー")
     console.log(paper)
   }
-
+ 
   const janken =()=>{
   const messages = ['グー','チョキ','パー'];
   const messageNo = Math.floor( Math.random() * 3);
   
   setCpuhand(messages[messageNo]);
+  winorlose()
   }
+  useEffect(() => {
+    axios.get('https://qiita.com/api/v2/items')
+    .then(res =>  {
+        console.log(res,'res check') 
+        
+    })
+  　},[])
   console.log(cpuhand)
   return (
     <div>
@@ -85,6 +121,9 @@ const App = () => {
         <div>
         相手の手{cpuhand}
       </div>
+      <div>
+        勝敗:{judge}
+      </div>
        <form name="OHGI72">
        <input id="starplatinam"type="text" onChange={e=>setTexter(e.target.value)}>
        </input>
@@ -98,7 +137,7 @@ const App = () => {
        <Button onClick={wanabeeee}>いいね</Button>
         <Button onClick={ureeeyyy}>ダメだね</Button>
         <Button onClick={overdrive}>reset</Button>{count}
-
+        {axios}
        </div>
 
     </div>    
